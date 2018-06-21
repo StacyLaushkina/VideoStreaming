092df5ade2ba0b0f9aa39145c867473bbfd1bbf5
Anastasia <Stacy.laushkina@gmail.com>
2018-06-21 10:06:35 +0700

    Add playback controls

--- app/src/main/java/laushkina/com/videostreaming/MainActivity.java
+++ app/src/main/java/laushkina/com/videostreaming/MainActivity.java
@@ -2,14 +2,9 @@ package laushkina.com.videostreaming;
 
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,18 +18,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialize(){
-        TextView startButton = findViewById(R.id.start_video_button);
-        startButton.setOnClickListener((view) -> startVideo());
+        getStartButton().setOnClickListener((view) -> startVideo());
     }
 
     private void startVideo(){
+        // Initialize video
         VideoView videoView = findViewById(R.id.video_view);
         videoView.setVideoURI(getVideoUri());
+
+        // Initialize playback controls
+        MediaController videoControl = new MediaController(this);
+        videoControl.setAnchorView(videoView);
+        videoView.setMediaController(videoControl);
+
         videoView.start();
+        disableStartButton();
+    }
+
+    private void disableStartButton(){
+        Button startButton = getStartButton();
+        startButton.setEnabled(false);
+        startButton.setAlpha(0.5f);
     }
 
     private Uri getVideoUri(){
         String vidAddress = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
         return Uri.parse(vidAddress);
+    }
+
+    private Button getStartButton(){
+        return findViewById(R.id.start_video_button);
     }
 }
